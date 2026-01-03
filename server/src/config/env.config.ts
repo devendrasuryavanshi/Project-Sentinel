94d7d136fe3b34ac5258823e31ab1109fd9b773d
Devendra Suryavanshi <devendrasooryavanshee@gmail.com>
2026-01-03 21:03:02 +0530

    feat: update API base URL configuration to use environment variables

--- server/src/config/env.config.ts
+++ server/src/config/env.config.ts
@@ -11,7 +11,7 @@ export class EnvConfig {
   public static readonly EMAIL_USER = process.env.EMAIL_USER as string;
   public static readonly EMAIL_PASS = process.env.EMAIL_PASS as string;
   public static readonly CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
-  public static readonly API_URL = process.env.API_URL || 'http://localhost:5000';
+  public static readonly API_URL = process.env.API_URL || 'http://localhost:5000/api';
   public static readonly SUPER_ADMIN_EMAIL = process.env.SUPER_ADMIN_EMAIL || "devendrasooryavanshee@gmail.com";
   public static readonly NODE_ENV = process.env.NODE_ENV || 'development';
 }
